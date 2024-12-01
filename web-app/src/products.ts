eac940a5f90816b16b41d93e959eff9509e3296b
Ansif M Shamsu <ansifofficialacc@gmail.com>
2024-12-01 20:23:52 +0530

    First Draft Home Page

---
+++ web-app/src/products.ts
@@ -0,0 +1,16 @@
+const products = [
+    {
+        title: "Dash & Tyre Polish",
+        subtitle: "Polish your Car Dash and Tyres, Make it as dark as sexy",
+        price: 299,
+        image: "/polish.jpg"
+    },
+    {
+        title: "Washing Shampoo",
+        subtitle: "Clean your vehicles with little effort",
+        price: 199,
+        image: "/shampoo.jpg"
+    }
+]
+
+export default products 
